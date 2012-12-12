cbad77aa38e6335c95ebadfcc6ee07ad61fc4f0d
arata <contact@materializing.net>
2012-12-12 13:50:25 +0900

    公開ページ用のリンクを追加

--- views/elements/admin/submenus/bcforum_posts_index.php
+++ views/elements/admin/submenus/bcforum_posts_index.php
@@ -5,6 +5,9 @@
 			<li>
 				<?php $bcBaser->link('スレッド一覧を表示する', array('plugin' => 'bcforum', 'controller' => 'bcforum_posts', 'action' => 'index')) ?>
 			</li>
+			<li>
+				<?php $bcBaser->link('公開ページ確認', '/' . 'bcforum' . '/index') ?>
+			</li>
 		</ul>
 	</td>
 </tr>
